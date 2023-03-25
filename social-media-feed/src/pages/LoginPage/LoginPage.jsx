eacce01b109d48eb6ca7eec71e2ed4a7e14f417b
drmodun <jan.modun.st@gmail.com>
2023-03-25 16:29:21 +0100

    started addding login system

--- social-media-feed/src/pages/LoginPage/LoginPage.jsx
+++ social-media-feed/src/pages/LoginPage/LoginPage.jsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
+import classes from "./LoginPage.module.css";
 export const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+    }
     //login logic
     return (
         <div>
-            <h1>Login Page</h1>
+            <h1>Login</h1>
+            <form onSubmit={(e)=>handleSubmit}>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <button type="submit">Login</button>
+            </form>
+            {error && <p>{error}</p>}
+
+
         </div>
     );
 };
